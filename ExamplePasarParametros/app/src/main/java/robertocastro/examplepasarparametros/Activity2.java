898cc5d2e5aad9d5c4b6357e25cc959740616f99
RoberthoCastro <robertocastro549@gmail.com>
2016-06-06 17:40:42 -0600

    Ejemplo de paso de parametros de un Activity a otro

---
+++ ExamplePasarParametros/app/src/main/java/robertocastro/examplepasarparametros/Activity2.java
@@ -0,0 +1,26 @@
+package robertocastro.examplepasarparametros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class Activity2 extends AppCompatActivity {
+
+    WebView webView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+        webView1 = (WebView)findViewById(R.id.webView);
+
+        Bundle bundle = getIntent().getExtras();
+        String dato = bundle.getString("pagina");
+        webView1.loadUrl("http://"+dato);
+    }
+
+    public void finalizar(View v){
+        finish();
+    }
+}
